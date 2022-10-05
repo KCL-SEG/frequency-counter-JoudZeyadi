09c6416640d43b47b062d887fd3661ccb3bf85e8
Joud Zeyadi <joud.zeyadi@kcl.ac.uk>
2022-10-05 20:45:30 +0100

    Solution 1

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,23 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for i in range(len(items)):
+        items[i] = str(items[i])
+
+    count = 0
+    for i in items:
+        count = 0
+        for j in items:
+            temp = i
+            if j == i:
+                count += 1
+        frequencies[i] = count
+    print(count)
+    print(temp)
+
+    #frequencies[items[i]] = count
     return frequencies
+
+print(frequencies(['a', 'b', 'a', 'b', 'b', 'c']))
+print(frequencies([100, 'Hello', '100', '100', 100]))
